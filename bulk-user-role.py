9be9609298fb13afdc8557b6e26131feb280f715
JTgnahZ <94643157+JTgnahZ@users.noreply.github.com>
2022-04-06 16:52:01 -0700

    Add files via upload

---
+++ bulk-user-role.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#Prisma SDWAN script to read from csv and bulk create tenant operator or update operator roles.
+#tiazhang@paloaltonetworks.com
+
+
+import cloudgenix
+import json
+import csv
+import os
+import sys
+import datetime
+import argparse
+import pprint
+
+# Global Vars
+SDK_VERSION = cloudgenix.version
+SCRIPT_NAME = 'CloudGenix: Get Operators'
+
+
+# Set NON-SYSLOG logging to use function name
+logger = logging.getLogger(__name__)
+
+try:
+    from cloudgenix_settings import CLOUDGENIX_AUTH_TOKEN
+
+except ImportError:
+    # will get caught below.
+    # Get AUTH_TOKEN/X_AUTH_TOKEN from env variable, if it exists. X_AUTH_TOKEN takes priority.
+    if "X_AUTH_TOKEN" in os.environ:
+        CLOUDGENIX_AUTH_TOKEN = os.environ.get('X_AUTH_TOKEN')
+    elif "AUTH_TOKEN" in os.environ:
+        CLOUDGENIX_AUTH_TOKEN = os.environ.get('AUTH_TOKEN')
+    else:
+        # not set
+        CLOUDGENIX_AUTH_TOKEN = None
+
+try:
+    from cloudgenix_settings import CLOUDGENIX_USER, CLOUDGENIX_PASSWORD
+
+except ImportError:
+    # will get caught below
+    CLOUDGENIX_USER = None
+    CLOUDGENIX_PASSWORD = None
+
+
+def go():
+############################################################################
+# Begin Script, parse arguments.
+############################################################################
+
+# Parse arguments
+bulkcsv = sys.argv[1]
+
+sys.path.append(os.getcwd())
+
+
+#Creating system role and customer role maps
+tenant_roles = []
+tenant_rolemap = {}
+
+resp = cgx_session.get.base_roles()
+
+if resp.cgx_status:
+    sysroles = resp.cgx_content.get("items", None)
+    for role in sysroles:
+        base_rolemap = {role["name"]: {"name": role["name"]}}
+        tenant_roles.append(role["name"])
+        tenant_rolemap.update(base_rolemap)
+else:
+    print("ERR: Could not retrieve base roles")
+    cloudgenix.jd_detailed(resp)
+    
+# Create custom roles and update custom rolemap
+custom_roles = []
+custom_rolemap = {}
+
+resp = cgx_session.get.roles()
+
+if resp.cgx_status:
+    roles = resp.cgx_content.get("items", None)
+    for role in roles:
+        rolemap = {role["name"]: {"id": role["id"]}}
+        custom_roles.append(role["name"])
+        custom_rolemap.update(rolemap)
+else:
+    print("ERR: Could not retrieve base roles")
+    cloudgenix.jd_detailed(resp[name])
+    
+    
+#check existing operators and generate key field list to avoid deuplication
+resp = cgx_session.get.tenant_operators()
+
+if resp.cgx_status:
+    existing_users = resp.cgx_content.get("items",None)
+    email_list = []
+    for user in existing_users:
+        email_list.append(user["email"])
+else:
+    print("ERR: Could not retrieve base roles")
+    cloudgenix.jd_detailed(resp)
+    
+
+# Read and validate roles from CSV
+# Only validate if email is in csv and no duplication in existing operators. More key field to be defined.
+with open(bulkcsv, 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    
+    for data in csv_reader:
+    
+        roles = data["roles"]
+        email = data["email"]        
+        
+        signup_data = {
+            "first_name": [],
+            "email": [],
+            "secondary_emails": [],
+            "phone_numbers": [],
+            "roles": [],
+            "custom_roles": [],
+            "disabled": False,
+            "disable_idp_login": True,
+            "enable_session_ip_lock": False,
+            "ipv4_list": [],
+            "last_name": [],
+            "password": [],
+            "repeatPassword": []
+        }
+        
+        update_data = {
+            "custom_roles": [],
+            "roles": [],
+            "_etag": 1
+        }
+        
+        signup_data["first_name"] = data["first_name"]
+        signup_data["last_name"] = data["last_name"]
+        signup_data["email"] = data["email"]
+        signup_data["password"] = data["password"]
+        signup_data["repeatPassword"] = data["password"]
+        
+        #Parse and split role dict if necessary
+        if roles:
+            if "," in roles:
+                tmp = roles.split(",")
+
+                for role in tmp:
+                    if role not in tenant_roles:
+                        if role not in custom_roles:
+                            print("ERR: Invalid role. Please input defined custom roles")
+                            sys.exit()
+                        else:
+                            mappedrole = custom_rolemap[role]
+                            signup_data["custom_roles"].append(mappedrole)
+                            update_data["custom_roles"].append(mappedrole)
+                                
+                    else:
+                        mappedrole = tenant_rolemap[role]
+                        signup_data["roles"].append(mappedrole)
+                        update_data["roles"].append(mappedrole)
+            else:
+                if roles not in tenant_roles:
+                    if roles not in custom_roles:
+                        print("ERR: Invalid role. Please input defined custom roles")
+                        sys.exit()
+                    else:
+                        mappedrole = custom_rolemap[roles]
+                        signup_data["custom_roles"].append(mappedrole)
+                        update_data["custom_roles"].append(mappedrole)
+                            
+                else:
+                    mappedrole = tenant_rolemap[roles]
+                    signup_data["roles"].append(mappedrole)
+                    update_data["roles"].append(mappedrole)
+        
+        #If email exsits in CSV
+        #-Check if email address already exist in system, if yes, update user roles only
+        #-If email is not existed in system, sign-up a new user
+        #If email doesn't exist in CSV, display error message and exit script.
+        if email:
+            if email in email_list:
+                #Retrieve operator ID and etag
+                resp = cgx_session.get.tenant_operators()
+                if resp.cgx_status:
+                    existing_users = resp.cgx_content.get("items",None)
+                    for user in existing_users:
+                        if email in user.values():
+                            oid = user["id"]
+                            update_data.update({"_etag" : user["_etag"]})
+                            pp.pprint(oid)
+                            pp.pprint(update_data)
+                            resp = cgx_session.patch.tenant_operators(operator_id = oid, data=update_data)
+                            cloudgenix.jd_detailed(resp)
+            else:
+                resp = cgx_session.post.signup(signup_data)
+                if resp.cgx_status:
+                    oid = resp.cgx_content.get("id", None)
+                    print("Operator created. ID: {}".format(oid))
+                else:
+                    print("ERR: Could not create Operator")
+        else:
+            print("ERR: Need primary email to create user account. Please make sure all new users input emails. ")
+            sys.exit()
